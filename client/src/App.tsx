d05421d60b2e9ed61e86ecf269502f4210d5a330
sreecharan-desu <sreecharan309@gmail.com>
2024-12-08 06:48:05 +0530

    feat: add transaction history, protected routes and 404 page - Add TransactionHistory page with responsive design - Add ProtectedRoute component for auth protection - Add NotFound page for 404 errors - Update UserMenu to include transaction history link - Update App routes configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { Profile } from './pages/Profile';
-import { CreateReward } from './pages/CreateReward';
-import { MyRewards } from './pages/MyRewards';
-import { RewardDetails } from './pages/RewardDetails';
-import { EditReward } from './pages/EditReward';
-import { Documentation } from './pages/Documentation';
-import { TransactionHistory } from './pages/TransactionHistory';
-import { NotFound } from './pages/NotFound';
-import { ProtectedRoute } from './components/ProtectedRoute';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AuthContext';
+import { AppRoutes } from './routes';
+import { Navbar } from './components/Navbar';
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/documentation" element={<Documentation />} />
-                <Route path="/rewards/:id" element={<RewardDetails />} />
-                
-                {/* Protected Routes */}
-                <Route path="/profile" element={
-                    <ProtectedRoute>
-                        <Profile />
-                    </ProtectedRoute>
-                } />
-                <Route path="/rewards/create" element={
-                    <ProtectedRoute>
-                        <CreateReward />
-                    </ProtectedRoute>
-                } />
-                <Route path="/rewards/:id/edit" element={
-                    <ProtectedRoute>
-                        <EditReward />
-                    </ProtectedRoute>
-                } />
-                <Route path="/my-rewards" element={
-                    <ProtectedRoute>
-                        <MyRewards />
-                    </ProtectedRoute>
-                } />
-                <Route path="/transactions" element={
-                    <ProtectedRoute>
-                        <TransactionHistory />
-                    </ProtectedRoute>
-                } />
-                
-                {/* 404 Route */}
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <AuthProvider>
+                <Toaster position="top-right" />
+                <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+                    <Navbar />
+                    <main className="container mx-auto px-4 py-8">
+                        <AppRoutes />
+                    </main>
+                </div>
+            </AuthProvider>
         </Router>
     );
 }
